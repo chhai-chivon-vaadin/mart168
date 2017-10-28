f16f7794c75efee2c543deb2f8b3d65249cda70d
chhaichivon <chivonchhai@hotmail.com>
2017-10-28 11:29:58 +0700

    add theme

--- mart168-app/src/main/java/com/mart168/mart168app/ui/MainUI.java
+++ mart168-app/src/main/java/com/mart168/mart168app/ui/MainUI.java
@@ -1,5 +1,6 @@
 package com.mart168.mart168app.ui;
 
+import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Label;
@@ -9,6 +10,7 @@ import com.vaadin.ui.UI;
  * @author by chhai.chivon  on 10/28/2017.
  */
 @SpringUI
+@Theme("mytheme")
 public class MainUI extends UI{
     @Override
     protected void init(VaadinRequest vaadinRequest) {
